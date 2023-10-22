0bac2d2c687400af7bf28ee6d54481a5304ad152
Edward Smale <essmale2005@gmail.com>
2023-10-22 19:45:44 +0100

    Update language

--- src/routes/(k)/t/js/+page.svelte
+++ src/routes/(k)/t/js/+page.svelte
@@ -12,9 +12,8 @@
 
     JavaScript was the first language I used after <a href="https://scratch.mit.edu">Scratch</a>, around the age of 11.
     <br><br>
-    I am very proficient in JavaScript, as it's the language I am most comfortable with and work best with.
-    <br><br>
-    I have used JavaScript basically everywhere you can find it (as per <a href="https://jayaprabhakar.medium.com/rethinking-atwoods-law-64a894b54aa4">Atwood's Law</a>),
+    I am very proficient in JavaScript, as it's the language I am most comfortable with and work best with. I have used
+    it basically everywhere you can find it (as per <a href="https://jayaprabhakar.medium.com/rethinking-atwoods-law-64a894b54aa4">Atwood's Law</a>),
     using Node.JS, Deno, <a href="/t/tauri">Tauri</a>, <a href="/t/electron">Electron</a>, on the web, etc.
     <br><br>
     <span class="text-2xl">TypeScript</span>
@@ -22,6 +21,6 @@
     I found <a href="https://typescriptlang.org">TypeScript</a> through the YouTuber <a href="https://www.youtube.com/watch?v=ahCwqrYpIuM">Fireship</a>
     as well as some friends who were also using it.
     <br><br>
-    I found that TypeScript completely and permanently changed the way I thought about and wrote JavaScript, and overall
-    I think it was the best change I ever made to my workflow.
+    I found that TypeScript completely and permanently changed the way I thought about and wrote JavaScript, and overall it was the best change I ever
+    made to my workflow.
 </Tool>

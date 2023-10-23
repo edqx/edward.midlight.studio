d313fc096dc3ed23329e6660df5d906c5d7dd1d2
Edward Smale <essmale2005@gmail.com>
2023-10-23 23:18:14 +0100

    Add image animation on gallery load

--- src/routes/(k)/Gallery.svelte
+++ src/routes/(k)/Gallery.svelte
@@ -3,17 +3,25 @@
 
     export let name: string;
     export let imageSrcs: string[];
-
-    let selectedImage = "";
-
+    
     let innerWidth = 0;
     let innerHeight = 0;
+    let areAllLoaded = false;
+    let loadedImages: boolean[] = [];
+
+    function onImageLoaded(idx: number) {
+        loadedImages[idx] = true;
+        if (loadedImages.length === imageSrcs.length && loadedImages.every(x => x)) {
+            areAllLoaded = true;
+        }
+        loadedImages = loadedImages;
+    }
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight></svelte:window>
 
 <div class="grid grid-cols-2 lg:grid-cols-3 grid-rows-auto gap-1">
-    {#each imageSrcs as imageSrc}
-        <GalleryImage {name} {imageSrc}/>
+    {#each imageSrcs as imageSrc, imageIdx}
+        <GalleryImage {name} {imageSrc} {imageIdx} totalCols={innerWidth >= 1024 ? 3 : 2} {areAllLoaded} on:loaded={() => onImageLoaded(imageIdx)}/>
     {/each}
 </div>
--- src/routes/(k)/GalleryImage.svelte
+++ src/routes/(k)/GalleryImage.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+    import { createEventDispatcher } from "svelte";
+    const dispatchEvent = createEventDispatcher();
 
     export let name: string;
     export let imageSrc: string;
+    export let imageIdx: number;
+    export let totalCols: number;
+    export let areAllLoaded: boolean;
 
     let selectedImage = "";
 
@@ -12,17 +16,44 @@
     let imageWidth = 0;
     let imageHeight = 0;
 
-    let imageElement: HTMLImageElement|undefined = undefined;
+    let imageOpacity = 0;
+
+    $: imageRow = Math.floor(imageIdx / totalCols);
+    $: imageCol = imageIdx % totalCols;
+
+    let thumbImageElement: HTMLImageElement|undefined = undefined;
+    let popupImageElement: HTMLImageElement|undefined = undefined;
+
+    $: if (areAllLoaded) {
+        setTimeout(() => {
+            function nextFrame() {
+                imageOpacity += (1 - imageOpacity) / 20;
+                if (imageOpacity >= 0.95) {
+                    imageOpacity = 1;
+                    return;
+                }
+                requestAnimationFrame(nextFrame);
+            }
+            requestAnimationFrame(nextFrame);
+        }, 100 + imageRow * 50 + imageCol * 50);
+    }
+
+    function onThumbImageLoaded() {
+        if (thumbImageElement === undefined)
+            return;
+    
+        dispatchEvent("loaded");
+    }
 
     function onImageLoaded() {
-        if (imageElement === undefined || innerWidth === 0 || innerHeight === 0)
+        if (popupImageElement === undefined || innerWidth === 0 || innerHeight === 0)
             return;
 
         const viewAspectRatio = innerWidth / innerHeight;
-        const imageAspectRatio = imageElement.naturalWidth / imageElement.naturalHeight;
+        const imageAspectRatio = popupImageElement.naturalWidth / popupImageElement.naturalHeight;
 
         if (innerWidth > innerHeight) {
-            if (imageElement.naturalHeight > imageElement.naturalWidth) {
+            if (popupImageElement.naturalHeight > popupImageElement.naturalWidth) {
                 imageHeight = innerHeight * 0.9;
                 imageWidth = imageHeight * imageAspectRatio;
             } else {
@@ -30,7 +61,7 @@
                 imageHeight = imageWidth / imageAspectRatio;
             }
         } else {
-            if (imageElement.naturalHeight > imageElement.naturalWidth) {
+            if (popupImageElement.naturalHeight > popupImageElement.naturalWidth) {
                 imageHeight = innerHeight * 0.9;
                 imageWidth = imageHeight * imageAspectRatio;
             } else {
@@ -46,24 +77,36 @@
     }
 
     function recalculateImageSize() {
-        if (imageElement === undefined || innerWidth === 0 || innerHeight === 0)
+        if (popupImageElement === undefined || innerWidth === 0 || innerHeight === 0)
             return;
 
-        if (imageElement.complete && imageElement.naturalWidth > 0) {
+        if (popupImageElement.complete && popupImageElement.naturalWidth > 0) {
             onImageLoaded();
         } else {
-            imageElement.onload = onImageLoaded;
+            popupImageElement.onload = onImageLoaded;
         }
     }
 
-    $: imageElement, innerWidth, innerHeight, recalculateImageSize();
+    function prepareIntroAnimation() {
+        if (thumbImageElement === undefined)
+            return;
+
+        if (thumbImageElement.complete && thumbImageElement.naturalWidth > 0) {
+            onThumbImageLoaded();
+        } else {
+            thumbImageElement.onload = onThumbImageLoaded;
+        }
+    }
+
+    $: popupImageElement, innerWidth, innerHeight, recalculateImageSize();
+    $: thumbImageElement, prepareIntroAnimation();
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight></svelte:window>
 
 <button class="w-40 sm:w-80 h-20 sm:h-40 filter transition duration-500" on:click={() => selectedImage = imageSrc}>
     <div class="w-full h-full overflow-hidden">
-        <img class="object-none transition duration-500 transform hover:scale-115" alt={name + " preview"} src={imageSrc}/>
+        <img class="object-none transition duration-500 transform hover:scale-115" style="opacity: {imageOpacity};" alt={name + " preview"} src={imageSrc} bind:this={thumbImageElement}/>
     </div>
 </button>
 <button
@@ -73,7 +116,7 @@
     <img style="width: {imageWidth}px; height: {imageHeight}px;"
         alt={name + " preview"}
         src={selectedImage}
-        bind:this={imageElement}/>
+        bind:this={popupImageElement}/>
     <div class="text-light-900 flex items-center">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
             <path fill-rule="evenodd" d="M10 1a.75.75 0 01.75.75v1.5a.75.75 0 01-1.5 0v-1.5A.75.75 0 0110 1zM5.05 3.05a.75.75 0 011.06 0l1.062 1.06A.75.75 0 116.11 5.173L5.05 4.11a.75.75 0 010-1.06zm9.9 0a.75.75 0 010 1.06l-1.06 1.062a.75.75 0 01-1.062-1.061l1.061-1.06a.75.75 0 011.06 0zM3 8a.75.75 0 01.75-.75h1.5a.75.75 0 010 1.5h-1.5A.75.75 0 013 8zm11 0a.75.75 0 01.75-.75h1.5a.75.75 0 010 1.5h-1.5A.75.75 0 0114 8zm-6.828 2.828a.75.75 0 010 1.061L6.11 12.95a.75.75 0 01-1.06-1.06l1.06-1.06a.75.75 0 011.06 0zm3.594-3.317a.75.75 0 00-1.37.364l-.492 6.861a.75.75 0 001.204.65l1.043-.799.985 3.678a.75.75 0 001.45-.388l-.978-3.646 1.292.204a.75.75 0 00.74-1.16l-3.874-5.764z" clip-rule="evenodd" />
